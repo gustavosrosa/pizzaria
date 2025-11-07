5bde42b9ff40d246916a4a039dce3c59d436a09b
gustavosrosa <g.dsantosrosa@gmail.com>
2025-11-07 15:58:17 -0300

    FEAT: Created route put into controller multer

--- backend/src/controllers/product/create-product.controller.ts
+++ backend/src/controllers/product/create-product.controller.ts
@@ -7,10 +7,15 @@ class CreateProductController {
 
         const { name, price, description, category_id } = req.body;
 
-        let banner = "";
-
         const createProductService = new CreateProductService();
-        const product = await createProductService.execute({ name, price, description, banner, category_id });
+
+        if (!req.file) {
+            throw new Error("Error upload file!");
+        }
+
+        const { originalname, filename } = req.file;
+
+        const product = await createProductService.execute({ name, price, description, banner: "", category_id });
 
         return res.json(product);
     }
