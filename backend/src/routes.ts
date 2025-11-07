863e22b5f87b53476411e7d93167652f6e2b96e7
gustavosrosa <g.dsantosrosa@gmail.com>
2025-11-07 15:38:01 -0300

    FEAT: Created endpoint structure add products

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,10 @@
-import { Router, type Request, type Response } from 'express';
+import { Router } from 'express';
 import { CreateUserController } from './controllers/user/create-user.controller';
 import { AuthUserController } from './controllers/user/auth-user.controller';
 import { DetailUserController } from './controllers/user/detail-user.controller';
 import { isAuthenticated } from './middlewares/is-authenticated.middleware';
-import { CreateCategoryController } from './controllers/categories/create-category.controller';
 import { DetailCategoryController } from './controllers/categories/detail-category.controller';
+import { CreateProductController } from './controllers/product/create-product.controller';
 
 const router = Router();
 
@@ -14,7 +14,10 @@ router.post('/session', new AuthUserController().handle);
 router.get('/me', isAuthenticated, new DetailUserController().handle); // Middleware - Before call the controller
 
 /** Routes categories */
-router.post("/categories", isAuthenticated, new CreateCategoryController().handle);
 router.get("/categories", isAuthenticated, new DetailCategoryController().handle);
+
+/** Routes products  */
+router.post("/product", isAuthenticated, new CreateProductController().handle);
+
 
 export { router };
